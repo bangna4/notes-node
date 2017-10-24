6d71ec128afbd120b2efcfbb7fbf80bd37c58300
nik44 <nbang0506@gmail.com>
2017-10-24 12:40:17 +0530

    added lodash

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ console.log('Starting app');
 
 const fs =  require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 var user = os.userInfo();
 
 console.log(user);
+console.log(_.isString(true));
+console.log(_.isString("dffsd"));
+
+var filteredArray = _.uniq(['nik', '1', 'nik',1,3,2,2]);
+
+console.log(filteredArray);
 
 var res = notes.addNote();
 
